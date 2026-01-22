da1e5ff817efc50f7bdbc056395e39b2e9d6758d
wenhao <111180600+wenhao-nus@users.noreply.github.com>
2026-01-23 01:15:35 +0800

    Add the ability to store inputs and display them
    The chatbot will save all user inputs and display them in a numbered
    list using the "list" command.

--- src/main/java/Aeolian.java
+++ src/main/java/Aeolian.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Aeolian {
     public static void main(String[] args) {
@@ -12,12 +13,24 @@ public class Aeolian {
         final String GOODBYE_MESSAGE = HORIZONTAL_LINE
                 + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE;
 
+        ArrayList<String> listStore = new ArrayList<String>();
+
         System.out.println(GREETING_MESSAGE);
         String userInput = sc.nextLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println(HORIZONTAL_LINE + userInput + '\n' + HORIZONTAL_LINE);
+            if (userInput.equals("list")) {
+                System.out.print(HORIZONTAL_LINE);
+                for (int i = 0; i < listStore.size(); i++) {
+                    System.out.println(i+1 + ". " + listStore.get(i));
+                }
+                System.out.println(HORIZONTAL_LINE);
 
+            } else {
+                listStore.add(userInput);
+                System.out.println(HORIZONTAL_LINE + " added: " + userInput +
+                        '\n' + HORIZONTAL_LINE);
+            }
             userInput = sc.nextLine();
         }
 
