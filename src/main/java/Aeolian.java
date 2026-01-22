7165b1162a650f8d00fbb62c127011ced929a144
wenhao <111180600+wenhao-nus@users.noreply.github.com>
2026-01-23 00:47:03 +0800

    Add echo functionality to chatbot
    The chatbot first greets the user, then asks the user for input command.
    It will echo out the user's input command until the user keys in the
    command "bye".

--- src/main/java/Aeolian.java
+++ src/main/java/Aeolian.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Aeolian {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String greeting = "____________________________________________________________\n" +
                 " Hello! I'm Aeolian\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
 
         System.out.println(greeting);
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.print("____________________________________________________________\n");
+            System.out.println(userInput);
+            System.out.print("____________________________________________________________\n");
+
+            userInput = sc.nextLine();
+        }
+
+        String goodbye = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(goodbye);
+        sc.close();
     }
 }
