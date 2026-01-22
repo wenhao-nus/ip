2430e442e276639a1f6b6578edd9a3f3b97c22ac
wenhao <111180600+wenhao-nus@users.noreply.github.com>
2026-01-22 14:27:57 +0800

    Add greeting and rename chatbot from Duke to Aeolian

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Aeolian User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Aeolian.java
@@ -0,0 +1,12 @@
+public class Aeolian {
+    public static void main(String[] args) {
+        String greetings = "____________________________________________________________\n" +
+                " Hello! I'm Aeolian\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(greetings);
+    }
+}
