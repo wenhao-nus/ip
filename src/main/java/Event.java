c36ad38129645add4e663cd37c637662e128f51b
wenhao <111180600+wenhao-nus@users.noreply.github.com>
2026-01-30 13:11:24 +0800

    Add save to disk functionality

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,14 @@ public class Event extends Task {
         this.to = to;
     }
 
+    public String getTo() {
+        return this.to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from
